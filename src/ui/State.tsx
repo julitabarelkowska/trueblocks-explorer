4d796be46842f11688e00e2e89175b026bf7f090
Dawid Szlachta <git@makingprogress.dev>
2021-08-19 11:58:28 +0200

    Typed GlobalState

--- src/ui/State.tsx
+++ src/ui/State.tsx
@@ -1,23 +1,86 @@
+import { toSuccessfulData, useCommand } from '@hooks/useCommand';
+import { Accountname, address as Address } from '@modules/types';
 import Cookies from 'js-cookie';
 import React, { createContext, useContext, useReducer } from 'react';
 import { ReactNode } from 'react-markdown';
 
-const GlobalStateContext = createContext<any[]>([]);
-
 const THEME = Cookies.get('theme');
 const ADDRESS = Cookies.get('address');
 
-const initialState = {
-  theme: THEME || null,
-  currentAddress: ADDRESS || null,
-  namesMap: null,
-  namesArray: null,
+type State = {
+  theme?: string,
+  currentAddress?: string,
+  namesMap?: Map<Address, Accountname>
+  namesArray?: Accountname[],
+  namesEditModal: boolean,
+  transactions: ReturnType<typeof useCommand>,
+  totalRecords: number,
+}
+
+const createDefaultTransaction = () => toSuccessfulData({
+  data: [], meta: {},
+});
+
+const initialState: State = {
+  theme: THEME,
+  currentAddress: ADDRESS,
+  namesMap: new Map(),
+  namesArray: [],
   namesEditModal: false,
-  transactions: null,
-  totalRecords: null,
+  transactions: [createDefaultTransaction(), false],
+  totalRecords: 0,
 };
 
-const GlobalStateReducer = (state: any, action: any) => {
+type SetTheme = {
+  type: 'SET_THEME',
+  theme: Pick<State, 'theme'>,
+};
+
+type SetCurrentAddress = {
+  type: 'SET_CURRENT_ADDRESS',
+  address: State['currentAddress'], // Pick<State, 'currentAddress'>,
+};
+
+type SetNamesMap = {
+  type: 'SET_NAMES_MAP',
+  namesMap: State['namesMap'],
+};
+
+type SetNamesArray = {
+  type: 'SET_NAMES_ARRAY',
+  namesArray: State['namesArray'],
+};
+
+type SetNamesEditModal = {
+  type: 'SET_NAMES_EDIT_MODAL',
+  val: State['namesEditModal'],
+};
+
+type SetTransactions = {
+  type: 'SET_TRANSACTIONS',
+  transactions: State['transactions'],
+};
+
+type SetTotalRecords = {
+  type: 'SET_TOTAL_RECORDS',
+  records: State['totalRecords'],
+};
+
+type GlobalAction =
+  | SetTheme
+  | SetCurrentAddress
+  | SetNamesMap
+  | SetNamesArray
+  | SetNamesEditModal
+  | SetTransactions
+  | SetTotalRecords;
+
+const GlobalStateContext = createContext<[
+  typeof initialState,
+  React.Dispatch<GlobalAction>
+]>([initialState, () => { }]);
+
+const GlobalStateReducer = (state: any, action: GlobalAction) => {
   switch (action.type) {
     case 'SET_THEME':
       Cookies.set('theme', action.theme);
@@ -26,7 +89,7 @@ const GlobalStateReducer = (state: any, action: any) => {
         theme: action.theme,
       };
     case 'SET_CURRENT_ADDRESS':
-      Cookies.set('address', action.address);
+      Cookies.set('address', action.address || '');
       if (action.address !== state.address) {
         return {
           ...state,
@@ -122,6 +185,7 @@ export const useGlobalState = () => {
 export const GlobalStateProvider = ({ children }: { children: ReactNode }) => {
   const [state, dispatch] = useReducer(GlobalStateReducer, initialState);
 
+  // TODO: wrap [state, dispatch] into useMemo for better performance (test it and note the diff!)
   return <GlobalStateContext.Provider value={[state, dispatch]}>{children}</GlobalStateContext.Provider>;
 };
 
