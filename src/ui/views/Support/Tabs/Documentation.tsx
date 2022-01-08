b9befa89a54db008da8850c041a8ec226d409a96
tjayrush <jrush@quickblocks.io>
2022-01-07 20:21:25 -0500

    Fixing doc links

--- src/ui/views/Support/Tabs/Documentation.tsx
+++ src/ui/views/Support/Tabs/Documentation.tsx
@@ -4,11 +4,11 @@ import ReactMarkdown from 'react-markdown';
 const documentation: string = `
 #### Documentation
 
-* All of our documentation is on [this site](https://docs.trueblocks.io).
-* Introductory text is [here](https://docs.trueblocks.io/docs/prologue/introduction/).
-* Documentation about our command line tool **chifra** is [here](https://docs.trueblocks.io/docs/prologue/introducing-chifra/).
-* Help for the TrueBlocks Account Explorer (this application) is [here](https://docs.trueblocks.io/docs/explorer/getting-started/).
-* Our blog is [here](https://docs.trueblocks.io/blog/).
+* All of our documentation is on [this site](https://trueblocks.io).
+* Introductory text is [here](https://trueblocks.io/docs/).
+* Documentation about our command line tool **chifra** is [here](https://trueblocks.io/docs/using/introducing-chifra/).
+* Help for the TrueBlocks Account Explorer (this application) is [here](https://trueblocks.io/docs/explorer/).
+* Our blog is [here](https://trueblocks.io/blog/).
 `;
 
 export const Documentation = () => (
