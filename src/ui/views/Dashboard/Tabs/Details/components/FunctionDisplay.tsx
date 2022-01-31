58098a82e0554d2f45432cef3636f9584bc02af6
tjayrush <jrush@quickblocks.io>
2022-01-31 04:51:59 -0500

    Small cleanup

--- src/ui/views/Dashboard/Tabs/Details/components/FunctionDisplay.tsx
+++ src/ui/views/Dashboard/Tabs/Details/components/FunctionDisplay.tsx
@@ -9,32 +9,37 @@ export const FunctionDisplay = ({ func, rawBytes }: { func: Function, rawBytes: 
 
   if (!func) return <></>;
 
+  // TODO: This hugely horrible thing is temporary -- the real one has to handle irrelevant
+  // TODO: parameters and tuple parameters
   const thing = (
     <table style={{ width: '100%' }}>
-      {Object.entries(func).map(
-        ([name, value]) => {
-          if (name === 'name') {
-            return (
-              <tr key={name}>
-                <td colSpan={3} className={styles.header} key={1}>{`Function: ${value}`}</td>
-              </tr>
-            );
-          }
-          return (
-            <>
-              {Object.entries(value).map(([key, val]) => (
-                <tr key={`${name}2`}>
-                  <td key={`${name}1`} style={{ width: '1%' }} />
-                  <td style={{ fontWeight: 'bold', width: '20%' }} key={`${name}2`}>{`${key}:`}</td>
-                  <td key={`${name}3`}>{`${val}`}</td>
+      <tbody>
+        {Object.entries(func).map(
+          ([name, value]) => {
+            if (name === 'name') {
+              return (
+                <tr key={`${name}1`}>
+                  <td colSpan={3} className={styles.header} key={1}>{`Function: ${value}`}</td>
                 </tr>
-              ))}
-            </>
-          );
-        },
-      )}
+              );
+            }
+            return (
+              <>
+                {Object.entries(value).map(([key, val]) => (
+                  <tr key={`${key}2`}>
+                    <td key={`${name}11`} style={{ width: '1%' }} />
+                    <td key={`${name}12`} style={{ fontWeight: 'bold', width: '20%' }}>{`${key}:`}</td>
+                    <td key={`${name}13`}>{`${val}`}</td>
+                  </tr>
+                ))}
+              </>
+            );
+          },
+        )}
+      </tbody>
     </table>
   );
+
   const articulated = (
     <pre style={{ overflowX: 'hidden' }}>
       {thing}
