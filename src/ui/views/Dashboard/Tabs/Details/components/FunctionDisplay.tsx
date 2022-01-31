05177a4931b6a66ea5b3095f107db75f85e1d770
tjayrush <jrush@quickblocks.io>
2022-01-31 17:39:27 -0500

    Function display

--- src/ui/views/Dashboard/Tabs/Details/components/FunctionDisplay.tsx
+++ src/ui/views/Dashboard/Tabs/Details/components/FunctionDisplay.tsx
@@ -7,45 +7,50 @@ import { Function } from '@sdk';
 export const FunctionDisplay = ({ func, rawBytes }: { func: Function, rawBytes: string }) => {
   const styles = useStyles();
 
-  if (!func) return <></>;
+  if (!func && (!rawBytes || rawBytes.length === 0)) return <></>;
 
-  // TODO: This hugely horrible thing is temporary -- the real one has to handle irrelevant
-  // TODO: parameters and tuple parameters
-  const thing = (
-    <table style={{ width: '100%' }}>
-      <tbody>
-        {Object.entries(func).map(
-          ([name, value]) => {
-            if (name === 'name') {
-              return (
-                <tr key={`${name}1`}>
-                  <td colSpan={3} className={styles.header} key={1}>{`Function: ${value}`}</td>
-                </tr>
-              );
-            }
-            return (
-              <>
-                {Object.entries(value).map(([key, val]) => (
-                  <tr key={`${name}${key}2`}>
-                    <td key={`${name}11`} style={{ width: '1%' }} />
-                    <td key={`${name}12`} style={{ fontWeight: 'bold', width: '20%' }}>{`${key}:`}</td>
-                    <td key={`${name}13`}>{`${val}`}</td>
+  let thing = <></>;
+  let articulated = <></>;
+
+  // TODO: This hugely horrible thing is temporary -- the real one has to
+  // TODO: handle irrelevant parameters and tuple parameters
+  if (func) {
+    thing = (
+      <table style={{ width: '100%' }}>
+        <tbody>
+          {Object.entries(func).map(
+            ([name, value]) => {
+              if (name === 'name') {
+                return (
+                  <tr key={`${name}1`}>
+                    <td colSpan={3} className={styles.header} key={1}>{`Function: ${value}`}</td>
                   </tr>
-                ))}
-              </>
-            );
-          },
-        )}
-      </tbody>
-    </table>
-  );
+                );
+              }
+              return (
+                <>
+                  {Object.entries(value).map(([key, val]) => (
+                    <tr key={`${name}${key}2`}>
+                      <td key={`${name}11`} style={{ width: '1%' }} />
+                      <td key={`${name}12`} style={{ fontWeight: 'bold', width: '20%' }}>{`${key.substr(0, 25)}:`}</td>
+                      <td key={`${name}13`}>{`${val}`}</td>
+                    </tr>
+                  ))}
+                </>
+              );
+            },
+          )}
+        </tbody>
+      </table>
+    );
 
-  const articulated = (
-    <pre style={{ overflowX: 'hidden' }}>
-      {thing}
-      <br />
-    </pre>
-  );
+    articulated = (
+      <pre style={{ overflowX: 'hidden' }}>
+        {thing}
+        <br />
+      </pre>
+    );
+  }
 
   const bytes = (
     <pre>
