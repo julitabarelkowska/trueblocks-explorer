82885705be9a87e5ba159e12cdfd5400c3c48185
Dawid Szlachta <git@makingprogress.dev>
2022-03-21 17:26:56 +0100

    Made DataDisplay faster when scrolling

--- src/ui/components/DataDisplay/index.tsx
+++ src/ui/components/DataDisplay/index.tsx
@@ -1,4 +1,6 @@
-import React, { useMemo } from 'react';
+import React, {
+  useCallback, useEffect, useState,
+} from 'react';
 
 import { Alert } from 'antd';
 
@@ -20,21 +22,44 @@ export function DataDisplay(
     data, onValue, onTooDeep = defaultOnTooDeep, showCopy = true,
   }: { data: {}, onValue: OnValue, onTooDeep?: () => JSX.Element, showCopy?: boolean},
 ) {
-  const treeModel = useMemo(() => makeTreeFromObject(data), [data]);
-  const combinedOnValue: OnValue = (path, value) => {
+  const combinedOnValue: OnValue = useCallback((path, value) => {
     if (value.kind === 'tooDeep') {
       return onTooDeep();
     }
 
     return onValue(path, value);
-  };
+  }, [onTooDeep, onValue]);
+
+  const [content, setContent] = useState<JSX.Element>(<></>);
+
+  useEffect(() => {
+    // When this component rerenders, we want to "cancel" (i.e. ignore) tree model
+    // and components. Otherwise, it would block when keyboard scrolling the transaction
+    // list fast.
+    let cancel = false;
+
+    // Making the main operation `async` forces JS engine to put it into another event loop,
+    // so it will not block. But we still need the ability to cancel it, because we don't
+    // want to show previous results if the user has already moved to another transaction.
+    (async () => {
+      const treeModel = makeTreeFromObject(data);
+      if (cancel) return;
+
+      const components = treeModelToComponents(treeModel, [], combinedOnValue);
+      if (cancel) return;
+
+      setContent(components);
+    })();
+
+    return () => { cancel = true; };
+  }, [combinedOnValue, data]);
 
   return (
     <div className='data-display'>
       { showCopy
         ? <CopyAsJson content={data} />
         : null}
-      {treeModelToComponents(treeModel, [], combinedOnValue)}
+      {content}
     </div>
   );
 }
